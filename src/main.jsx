387aa6f75146f2e2cabc94d9740049e4008b953f
Oluwakemi Raimi <oluwakemiraimi.nig@gmail.com>
2023-03-11 18:08:28 +0100

    create globalstyle

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Main from './MainPage'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Main from './MainPage';
 import { setup } from "goober";
+import { GlobalStyles } from './globalStyle';
 
 setup(React.createElement);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyles />
     <Main />
   </React.StrictMode>,
 )
