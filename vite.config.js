a7ffeb3e4899ad885a6e89c41aa831d5e7078541
Raimi Ademola <dev@binghr.io>
2023-03-28 02:50:37 +0100

    log dir name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
+console.log('__dirname:', __dirname);
+
 export default defineConfig({
   plugins: [svgr(), react()],
   build: {
