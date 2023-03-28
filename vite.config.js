a5c1a0a68e4489d97e9f0b5c97738fe0ad999b0b
Raimi Ademola <dev@binghr.io>
2023-03-28 02:24:41 +0100

    change build to build folder instead of dist

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [svgr(), react()],
+  build: {
+    outDir: 'build'
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
