0b1755ebe71f50d2da7210fc95148847df1140c7
Oluwakemi Raimi <oluwakemiraimi.nig@gmail.com>
2023-03-30 12:38:31 +0100

    wip

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,34 +1,53 @@
 import React from "react";
-import { styled } from "goober";
+import { styled, css } from "goober";
 
-const Container = styled("button")`
+const Container = styled("button")(
+  ({ padding, margin, bg, textcolor, border }) => css`
     display: flex;
     align-items: center;
     justify-content: center;
     gap: 0.625rem;
-    padding: 0.625rem;
-    background: ${props => props.bg};
-    color: ${props => props.textcolor};
-    border: ${(props) =>
-        props.border ? `1px solid ${props.border}` : "none"};
+    padding: ${padding ? "var(--padding)" : "0.625rem"};
+    margin: ${margin ? "var(--margin)" : "0"};
+    background: ${bg && "var(--bg)"};
+    color: ${textcolor && "var(--textcolor)"};
+    border: ${border ? `1px solid ${"var(--border)"}` : "none"};
     border-radius: var(--br);
     text-align: center;
     outline: none;
     font-family: inherit;
     cursor: pointer;
-    svg {
-        fill: ${props => props.iconColor};
-    }
-`;
+  `
+);
 
-const Button = ({ leftIcon, rightIcon, iconColor, textcolor, bg, border, children, className, onClick}) => {
-  return (
-    <Container className={`type-title3 ${className}`} bg={bg} textcolor={textcolor} border={border}onClick={onClick} iconColor={iconColor}>
-      {leftIcon && leftIcon}
-      {children}
-      {rightIcon && rightIcon}
-    </Container>
-  );
+const Button = ({
+    leftIcon,
+    rightIcon,
+    textcolor,
+    bg,
+    border,
+    margin,
+    padding,
+    children,
+    onClick
+}) => {
+    const containerClassName = css({
+    "--padding": padding,
+    "--margin": margin,
+    "--bg": bg,
+    "--textcolor": textcolor,
+    "--border": border
+    });
+    return (
+        <Container
+          className={`${containerClassName}`}
+          onClick={onClick}
+        >
+          {leftIcon && leftIcon}
+          {children}
+          {rightIcon && rightIcon}
+        </Container>
+    );
 };
 
 export default Button;
