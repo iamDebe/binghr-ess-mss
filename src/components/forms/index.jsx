bd36aa8f4b04f5263c37cb67c35e602eac4038f4
Debe <emmanueljoce@gmail.com>
2023-04-02 18:27:12 +0100

    error resolved

--- src/components/forms/index.jsx
+++ src/components/forms/index.jsx
@@ -1,5 +1,5 @@
-import CheckBox from "@/CheckBox";
-import SelectField from "@/SelectField";
-import TextField from "@/TextField";
+import CheckBox from "@/components/forms/CheckBox";
+import SelectField from "@/components/forms/SelectField";
+import TextField from "@/components/forms/TextField";
 
 export { CheckBox, SelectField, TextField };
