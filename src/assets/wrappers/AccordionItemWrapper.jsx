e7b77f919444114c9fe7fa5585bb85a47a34790d
Oluwakemi Raimi <oluwakemiraimi.nig@gmail.com>
2023-04-13 15:17:05 +0100

    fix tab designs

--- src/assets/wrappers/AccordionItemWrapper.jsx
+++ src/assets/wrappers/AccordionItemWrapper.jsx
@@ -1,8 +1,8 @@
 import { styled } from "goober";
 
 export const AccordionItem = styled("div")`
+  .title-box {
     padding: 0.75rem 0;
-    margin: 0 1.25rem;
     border-bottom: 1px solid var(--grey-100);
     background: var(--white);
     color: var(--grey-300);
@@ -10,5 +10,7 @@ export const AccordionItem = styled("div")`
     &.active {
         color: var(--grey-500);
         border-bottom: 4px solid var(--red-400);
+        margin-bottom: 0.75px;
     }
+  }
 `;
